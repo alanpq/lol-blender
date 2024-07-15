93cc8b62b09cd93b97f764230d7acc66f76df257
Alan Panayotov <alanpanayotov@gmail.com>
2024-07-15 20:51:12 +0100

    fix: remove test build artifacts

--- addon_test/lol_blender/addons/lol_blender/i18n/dictionary.py
+++
@@ -1,6 +0,0 @@
-from lol_blender.common.class_loader.auto_load import preprocess_dictionary
-
-dictionary = {
-}
-
-dictionary = preprocess_dictionary(dictionary)
--- addon_test/lol_blender/common/i18n/dictionary.py
+++
@@ -1,7 +0,0 @@
-common_dictionary = {
-    "zh_CN": {
-        ("*", "translation"): "翻译",
-    }
-}
-
-common_dictionary["zh_HANS"] = common_dictionary["zh_CN"]
