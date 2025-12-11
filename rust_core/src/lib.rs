7699ec91d1ac1d1b15574cd30ad2bec041c376e5
Alan Panayotov <alanpanayotov@gmail.com>
2025-12-11 19:59:18 +0000

    fix: add hot reload limitations to lib docs

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -1,3 +1,11 @@
+//! Hot reloadable core impl of our extension
+//! Only this can be hot reloaded.
+//!
+//! You must ensure any threads and resources are cleanly joined and dropped when loading a new version
+//!   - via `Drop` for our `Context` implementor `Impl`.
+//! Anything exposed to Python through `rust_wrap` should be defined in `rust_api` or `rust_hot`.
+//! New traits, functions, structs, enums, etc., and changes to them are **not reloadable**.
+
 use std::{
     collections::{BTreeMap, HashMap, VecDeque},
     fs::File,
